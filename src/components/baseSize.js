9d9248bf3b244bdf9badb7ec7711c9169334847d
zuotang <zuo.tang@outlook.com>
2019-08-29 10:38:47 +0800

    修改

--- src/components/baseSize.js
+++ src/components/baseSize.js
@@ -2,10 +2,11 @@ import React,{useEffect} from 'react';
 
 function baseSize(){
     useEffect(()=>{
-        function reSize(e){
+        function reSize(){
             var clientW = document.documentElement.clientWidth || document.body.clientWidth;
-            document.documentElement.style.fontSize=100*(clientW/375)+'px'
+            document.documentElement.style.fontSize=12*(clientW/375)+'px'
         }
+        reSize()
         window.addEventListener('resize',reSize);
         return ()=>{
             window.removeEventListener('resize',reSize);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,54 @@
-import React from "react"
-import { Link } from "gatsby"
+import React,{useEffect,useState,useRef} from "react"
 import baseSize from '../components/baseSize';
-import styled from 'styled-components'
-const Foot=styled.div`
-  position:fixed;
-  bottom:0;
-  left:0;
-  right:0;
-  display:flex;
-  font-size:0.12rem;
-  span{
-    margin:1rem;
-    font-size:12px
-  }
-`
-const Photo=styled.img`
-  width:1rem;
-`
+import { Canvas,useRender,extend,useThree } from 'react-three-fiber'
+import Layout from '../components/layout';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+extend({ OrbitControls })
+
+function Controls(props) {
+  const { camera, canvas } = useThree()
+  const controls = useRef()
+  useRender(() => controls.current && controls.current.update())
+  return <orbitControls ref={controls} args={[camera, canvas.parentNode]} {...props} />
+}
+
+
+const Box=()=>{
+  const [scale,setScale]=useState(1);
+  const [hovered,setHovered]=useState(false);
+  const boxRef=useRef();
+
+
+  useEffect(()=>{
+    document.body.style.cursor=hovered?"pointer":"default"
+  },[hovered])
+  useRender(()=>{
+    boxRef.current.rotation.x +=0.01;
+    boxRef.current.rotation.y +=0.01;
+  })
+  return (
+    <mesh ref={boxRef} scale={[scale,scale,scale]}   onClick={() => setScale(scale === 1 ? 1.5 : 1)}
+    onPointerOver={() => setHovered(true)}
+    onPointerOut={() => setHovered(false)}>
+      <boxBufferGeometry args={[2,2,2]} attach="geometry" />
+      <meshPhongMaterial attach="material"  color="teal"/>
+    </mesh>
+  )
+}
+
 const IndexPage = () => {
   baseSize()
   return (
- <div>
-     <Photo src="https://www.logosc.cn//uploads/output/2019/08/15/0e3bd3cb4dfc1419eee8e89f541a4efd.jpg?t=1565838329" />
-     <Photo src="https://www.logosc.cn//uploads/output/2019/08/14/0c21379f2d07243e59dd2c6bc9f7a217.jpg?t=1565792539" />
-     <Photo src="https://www.logosc.cn//uploads/output/2019/08/15/12c902c40458e243c810e60da00e5029.jpg?t=1565833730" />
-     <Photo src="https://www.logosc.cn//uploads/output/2019/08/14/12b11c5541ac24e59b9a003ab9958bd3.jpg?t=1565758724" />
-   <Foot>
-   
-     测试wenzi
-     <span>测试文字12px</span>
-   </Foot>
-
- </div>
+    <Layout>
+    <Canvas >
+    <Controls />
+      <pointLight distance={50} intensity={1.5} color="white" />
+      <spotLight intensity={0.5} position={[10, 10, 40]} penumbra={1} />
+      <Box />
+      
+    </Canvas>
+    </Layout>
 )}
 
 export default IndexPage
