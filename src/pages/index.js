96a0e971e6fcc0479921851e8df16a526d401b9e
zuotang <zuo.tang@outlook.com>
2019-09-17 20:10:00 +0800

    修改

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,10 @@ import React from "react"
 import Layout from '../components/layout'
 import A from '../components/a'
 import B from '../components/b'
+import useBaseSize from "../components/baseSize";
 
 const IndexPage = () => {
+  useBaseSize()
   return (
     <Layout>
       <A />
